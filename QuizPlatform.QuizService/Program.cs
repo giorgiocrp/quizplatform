951c1f966c0d2ab9350582c6b724b7ada4063f30
Giorgio <g.carpoca@gmail.com>
2025-04-01 10:10:15 +0200

    ancora quiz service

--- QuizPlatform.QuizService/Program.cs
+++ QuizPlatform.QuizService/Program.cs
@@ -1,41 +1,62 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.IdentityModel.Tokens;
+using QuizPlatform.QuizService;
+using QuizPlatform.QuizService.EndPointDefinitions;
+using QuizPlatform.QuizService.Extensions;
+using QuizPlatform.QuizService.Repositories.DbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var connectionString=builder.Configuration.GetConnectionString("QuizDataDb");
+builder.Services.AddDbContextPool<QuizDbContext>(options =>
+    options.UseNpgsql(connectionString, npgsqlBuilder =>
+            npgsqlBuilder.MigrationsAssembly(typeof(Program).Assembly.GetName().Name))
+        .ConfigureWarnings(warnings => warnings.Log(RelationalEventId.PendingModelChangesWarning)));
+builder.EnrichNpgsqlDbContext<QuizDbContext>(settings=>settings.DisableRetry=true);
+
+builder.AddRabbitMQClient("messaging");
+
+builder.Services.AddAuthorization();
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(x =>
+    {
+        x.Authority = builder.Configuration["Authentication:Authority"];
+        x.RequireHttpsMetadata = false;
+        x.Audience = builder.Configuration["Authentication:Audience"];
+        x.MetadataAddress = builder.Configuration["Authentication:MetaDataAddress"]!;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"]
+        };
+    });
+   
+builder.RegisterServices();
+
+builder.RegisterDependencies();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", cfg =>
+    {
+        cfg.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-app.UseHttpsRedirection();
+app.RegisterMiddlewares();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.RegisterProjectEndpoints();
